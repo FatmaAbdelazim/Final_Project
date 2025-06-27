8bb26ea0d075355c0fd6adf0747ee6bddcb75356
FatmaAbdelazim <fatmaalasiri15@gmail.com>
2025-06-27 18:11:03 +0300

    final navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { VolunteerRegisterComponent } from './features/volunteer/register/register.component';
+import { VoluntaryOrganizationRegisterComponent } from './features/voluntary_organization/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
 export const routes: Routes = [
-
+      {path:'', component: HomeComponent},
+      {path:'home', component: HomeComponent},
+      {path:'login', component: LoginComponent},
+      {path:'volunteer_register', component: VolunteerRegisterComponent},
+      {path:'voluntary_organization_register', component: VoluntaryOrganizationRegisterComponent},
+      {path:'about', component: AboutComponent},
 ];
