099103b2122ac3ecd3bca7143240397336d9c2fb
Rahma <Rahma@DESKTOP-ASKPTHG>
2025-06-30 02:29:59 +0300

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { VoluntaryOrganizationRegisterComponent } from './features/voluntary_org
 import { AboutComponent } from './pages/about/about.component';
 import { OpportunitiesComponent } from './pages/opportunities/opportunities.component';
 import { TeamsComponent } from './pages/teams/teams.component';
+import { VerifyCodeComponent } from './shared/components/verify-code/verify-code.component';
+import { ResetPasswordComponent } from './shared/components/reset-password/reset-password.component';
 export const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'home', component: HomeComponent },
@@ -14,6 +16,8 @@ export const routes: Routes = [
       { path: 'voluntary_organization_register', component: VoluntaryOrganizationRegisterComponent },
       { path: 'about', component: AboutComponent },
       { path: 'opportunities', component: OpportunitiesComponent },
-      { path: 'teams', component: TeamsComponent }
+      { path: 'teams', component: TeamsComponent },
+      { path: 'verify-code', component: VerifyCodeComponent },
+      { path: 'reset-pass', component: ResetPasswordComponent }
 
 ];
