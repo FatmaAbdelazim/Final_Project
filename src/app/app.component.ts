9b7af9044dc746ad4ea5cf6952abb41484f03438
Rahma <Rahma@DESKTOP-ASKPTHG>
2025-06-29 16:24:23 +0300

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { RegisterComponent } from "./features/volunteer/register/register.component";
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
