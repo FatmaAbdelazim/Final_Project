83f3772360654549fcaffbccb708aa04b47035a2
FatmaAbdelazim <fatmaalasiri15@gmail.com>
2025-07-09 00:50:29 +0300

    volunteerHome

--- src/app/features/volunteer/volunteer-home/volunteer-home.component.ts
+++ src/app/features/volunteer/volunteer-home/volunteer-home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { OppCardVoluteerComponent } from "../../../shared/components/opp-card-voluteer/opp-card-voluteer.component";
+import { OppCardSuggationVoluteerComponent } from "../../../shared/components/opp-card-suggation-voluteer/opp-card-suggation-voluteer.component";
 
 @Component({
   selector: 'app-volunteer-home',
-  imports: [],
+  imports: [OppCardVoluteerComponent, OppCardSuggationVoluteerComponent],
   templateUrl: './volunteer-home.component.html',
   styleUrl: './volunteer-home.component.css'
 })
---
+++ src/app/shared/components/opp-card-voluteer/opp-card-voluteer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opp-card-voluteer',
+  imports: [],
+  templateUrl: './opp-card-voluteer.component.html',
+  styleUrl: './opp-card-voluteer.component.css'
+})
+export class OppCardVoluteerComponent {
+
+}
