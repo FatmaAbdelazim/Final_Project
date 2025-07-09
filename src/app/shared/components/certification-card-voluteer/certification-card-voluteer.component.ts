29e0fc45cf32edaafe2f7a82ed13835c710569e3
FatmaAbdelazim <fatmaalasiri15@gmail.com>
2025-07-09 05:01:37 +0300

    volunteerAttendanceandCertifications

---
+++ src/app/shared/components/certification-card-voluteer/certification-card-voluteer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certification-card-voluteer',
+  imports: [],
+  templateUrl: './certification-card-voluteer.component.html',
+  styleUrl: './certification-card-voluteer.component.css'
+})
+export class CertificationCardVoluteerComponent {
+
+}
